254c6e82fbf5a4f70edfa104145b5d8765348ebf
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-22 18:11:48 +0545

    adding bootstrap template

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ from home import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("about", views.about, name="about"),
-    path("contact", views.contact, name="contact"),
+    path("services", views.services, name="services"),
     path("blog", views.blog, name="blog"),
+    path("contact", views.contact, name="contact")
 ]
--- home/views.py
+++ home/views.py
@@ -2,19 +2,17 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    context={
-"variable": "I have passed a props in django",
-"value": 101,
-"lolipop": "django is beautiful"
-    }
-    return render(request, "index.html", context)
+    return render(request, "index.html")
     # return HttpResponse("This is home page")
 
 def about(request):
     return HttpResponse("This is about page")
 
-def contact(request):
-    return HttpResponse("This is contact page")
+def services(request):
+    return HttpResponse("This is service page")
 
 def blog(request):
     return HttpResponse("This is blog page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
