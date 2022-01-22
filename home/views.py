25a18e2722c977462d4427e71a9de05880fcb902
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-22 17:43:11 +0545

    adding template

--- home/views.py
+++ home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is home page")
+    context={
+"variable": "I have passed a props in django",
+"value": 101,
+"lolipop": "django is beautiful"
+    }
+    return render(request, "index.html", context)
+    # return HttpResponse("This is home page")
 
 def about(request):
     return HttpResponse("This is about page")
