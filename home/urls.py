9676b0fcbe5b90d9812b7a6a3c398334357c65c4
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-22 13:55:41 +0545

    urls added

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,7 @@ from home import views
 
 urlpatterns = [
     path("", views.index, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("blog", views.blog, name="blog"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("This is home page")
+
+def about(request):
+    return HttpResponse("This is about page")
+
+def contact(request):
+    return HttpResponse("This is contact page")
+
+def blog(request):
+    return HttpResponse("This is blog page")
