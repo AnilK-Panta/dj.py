4a0406efbb29c918c129e469d43aa4326ee5da9b
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-24 02:51:56 +0545

    db msg display name

--- home/models.py
+++ home/models.py
@@ -7,3 +7,6 @@ class Contact(models.Model):
     phome=models.CharField(max_length=12)
     message=models.TextField()
     date=models.DateField()
+
+    def __str__(self):
+        return self.name
