e0f83eba3638cbea69a09d3d94f750ab72e77c9f
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-24 02:33:13 +0545

    connected to db and data stored in table

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 def index(request):
@@ -15,4 +17,12 @@ def blog(request):
     return render(request, "blog.html")
 
 def contact(request):
+    if request.method == "POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        phone=request.POST.get("phone")
+        message=request.POST.get("message")
+        contact=Contact(name=name, email=email, phome=phone, message=message, date=datetime.today())
+        contact.save()
+
     return render(request, "contact.html")
