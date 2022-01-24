5756859daab088dbd044688823d5f0898801eb82
Anil K. Panta <anilpanta2053@gmail.com>
2022-01-24 19:11:00 +0545

    messages.success

--- Hello/settings.py
+++ Hello/settings.py
@@ -128,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+
+from django.contrib.messages import constants as messages
--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime
 from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -23,6 +24,11 @@ def contact(request):
         phone=request.POST.get("phone")
         message=request.POST.get("message")
         contact=Contact(name=name, email=email, phome=phone, message=message, date=datetime.today())
-        contact.save()
+        
+        if(name or email or phone or message):
+            messages.success(request, 'Thankyou your message was sent')
+            contact.save()
+        else:
+            messages.warning(request, "Your field is empty")
 
     return render(request, "contact.html")
